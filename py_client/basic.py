db882c2e4f65743e28d2af5c7519de162e2641a1
AhmedARmohamed <mahmedrashid128@gmail.com>
2023-07-17 22:35:41 +0300

    Initial commit

---
+++ py_client/basic.py
@@ -0,0 +1,10 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
+
+get_request = requests.get(endpoint) # HTTP Request
+# print(get_request.json())
+print(get_request.text)
+print(get_request.status_code)
